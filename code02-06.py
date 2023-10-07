7ca79918726383a8b5c53bedbc61dee8c7001cd9
euna <127270717+chna8137@users.noreply.github.com>
2023-10-07 22:53:42 +0900

    23-10-07

--- code02-06.py
+++ code02-06.py
@@ -1,3 +1,5 @@
+#code02-06.py
+
 import turtle
 
 myT = None
@@ -5,7 +7,7 @@ myT = None
 myT = turtle.Turtle()
 myT.shape('turtle')
 
-for i in range(0, 4) :
+for i in range(0,4) : # for(int = 0, i < 4, i++)
     myT.forward(200)
     myT.right(90)
 
